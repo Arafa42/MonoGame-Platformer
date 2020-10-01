0b267274a0dc48862173d943144644269e677aba
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-10-01 18:31:05 +0200

    Monogame Project started
    -User input class added
    - Screensize added

---
+++ Platformer/Platformer/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Platformer.User_Interface;
+
+namespace Platformer
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Input inp;
+        const int SCREENWIDTH = 1024, SCREENHEIGHT = 768;
+        const bool FULLSCREEN = false;
+        PresentationParameters pp;
+        static public int screenW, screenH;
+        static public Vector2 screen_center;
+        Rectangle screenRect, desktopRect;
+        RenderTarget2D MainTarget;
+
+
+
+
+        public Game1()
+        {
+            int initial_screen_width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 10;
+            int initial_screen_height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 10;
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = initial_screen_width,
+                PreferredBackBufferHeight = initial_screen_height,
+                IsFullScreen = false,
+                PreferredDepthStencilFormat = DepthFormat.Depth16
+            };
+            Window.IsBorderless = true;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            inp = new Input();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            MainTarget = new RenderTarget2D(GraphicsDevice, SCREENWIDTH, SCREENHEIGHT);
+            pp = GraphicsDevice.PresentationParameters;
+            SurfaceFormat format = pp.BackBufferFormat;
+            screenW = MainTarget.Width;
+            screenH = MainTarget.Height;
+            desktopRect = new Rectangle(0, 0, pp.BackBufferWidth, pp.BackBufferHeight);
+            screenRect = new Rectangle(0, 0, screenW,screenH);
+            screen_center = new Vector2(screenW / 2.0f, screenH / 2.0f) - new Vector2(32f, 32f); 
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            // TODO: Add your update logic here
+            
+            inp.Update();
+            if (inp.Keypress(Keys.Escape)) { Exit(); }
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Platformer/Platformer/User Interface/Input.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Platformer.User_Interface
+{
+    class Input
+    {
+
+        public KeyboardState kb, old_kb;
+        public bool shift_down, ctrl_down, alt_down;
+        public bool shift_press, ctrl_press, alt_press;
+        public bool old_shift_down, old_ctrl_down, old_alt_down;
+
+        public Input() { }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Keypress(Keys k)
+        {
+            if (kb.IsKeyDown(k) && old_kb.IsKeyUp(k)) return true;
+            else return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Keydown(Keys k)
+        {
+            if (kb.IsKeyDown(k)) return true;
+            else return false;
+        }
+
+
+        public void Update()
+        {
+
+            old_alt_down = alt_down;
+            old_ctrl_down = ctrl_down;
+            old_shift_down = shift_down;
+            old_kb = kb;
+            kb = Keyboard.GetState();
+            shift_down = false;
+            ctrl_down = false;
+            alt_down = false;
+            shift_press = false;
+            ctrl_press = false;
+            alt_press = false;
+
+            if (kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift)) { shift_down = true; }
+            if (kb.IsKeyDown(Keys.LeftAlt) || kb.IsKeyDown(Keys.RightAlt)) { alt_down = true; }
+            if (kb.IsKeyDown(Keys.LeftControl) || kb.IsKeyDown(Keys.RightControl)) { ctrl_down = true; }
+            if (shift_down && !old_shift_down) { shift_press = true; }
+            if (alt_down && !old_alt_down) { alt_press = true; }
+            if (ctrl_down && !old_ctrl_down) { ctrl_press = true; }
+
+        }
+    }
+}
